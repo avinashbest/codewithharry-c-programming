0bcb6f0620e2cc702a7757b6017c5601120a424d
aaarif796 <48153674+aaarif796@users.noreply.github.com>
2020-10-14 10:34:51 +0530

    Update areacircle.c

--- AreaCircle.c
+++ AreaCircle.c
@@ -1,18 +1,17 @@
 #include <stdio.h>
-
+define PI 3.14;
 int main()
-{
+{   
+    //Area of circle
     float radius = 0.0, height = 0.0;
-    float PI = 3.14;
     printf("\nEnter the radius of circle: ");
     scanf("%f", &radius);
     printf("The area of the circle is %.3f sq. units\n", PI * radius * radius);
 
     /*Now calculating the volume of the cylinder attached to some height of the above circle data.*/
 
-    printf("\nEnter the height of cylinder connected to the circle: ");
+    printf("Enter the height of cylinder connected to the circle: ");
     scanf("%f", &height);
     printf("The volume of the cylinder is %.3f cubic units\n\n", PI * radius * radius * height);
-
     return 0;
 }
