e0416c06462032292dfe2e5881c51fd376ae3ffb
TechyAyush <69710917+TechyAyush@users.noreply.github.com>
2020-10-24 21:28:15 +0530

    Update armStrongNumber.c
    Added separate function for getting it is Armstrong or not.And some comments updated.!!

--- armStrongNumber.c
+++ armStrongNumber.c
@@ -1,21 +1,27 @@
-#include<stdio.h>
-/*The number which each number is cube is equal to orignial number then it's called armstrong number.*/
+#include <stdio.h>
+/*The number which each digit's cube is equal to orignial number then it's called armstrong number.*/
+void isArmstrong(int n) //Function for finding Armstrong
+{
+    int a, sum, rem;
+    a = n;
+    sum = 0;
+    while (n > 0)
+    {
+        rem = n % 10;
+        sum = sum + rem * rem * rem;
+        n = n / 10;
+    }
+    if (sum == a)
+        printf("%d is armstrong number", a);
+    else
+        printf("%d is not armstrong number", a);
+}
 int main()
 {
-    int n,a,sum,rem;
+    int n;
     printf("Enter the number to check where number is armstrong or not:");
-    scanf("%d",&n);
-    a=n;
-    sum=0;
-    while(n>0)
-    {
-        rem=n%10;
-        sum=sum+rem*rem*rem;
-        n=n/10;
-    }
-    if(sum==a)
-        printf("%d is armstrong number",a);
-    else
-        printf("%d is not armstrong number",a);
+    scanf("%d", &n);
+    isArmstrong(n);//calling function
+
     return 0;
 }
